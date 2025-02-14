7687b54d2c5b37cb246bd06b7e6f7fa2ee27bdfd
Juan Gonzalez <seminariopv@hotmail.com>
2025-02-14 12:16:28 -0600

    Add Hono server

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { reactRouterHonoServer } from "react-router-hono-server/dev";
+import { iconsSpritesheet } from "vite-plugin-icons-spritesheet";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     plugins: [
         reactRouter(),
-        tsConfigPaths()
+        tsConfigPaths(),
+        reactRouterHonoServer(),
+        iconsSpritesheet(
+            {
+                withTypes: true,
+                inputDir: "icons",
+                outputDir: "./public/icons",
+                fileName: "icons.svg",
+                formatter: "prettier",
+            }
+        ),
     ],
 })
