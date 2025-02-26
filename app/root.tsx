e87205d7d881b1516805eb29866d607c3b4c1e64
Juan Gonzalez <seminariopv@hotmail.com>
2025-02-26 07:16:35 -0600

    Add RR7 missing upgrade changes

--- app/root.tsx
+++ app/root.tsx
@@ -1,31 +1,47 @@
-import { type LinksFunction, Links, Meta, Outlet, Scripts, ScrollRestoration, useLoaderData } from "react-router";
-import { useTranslation } from "react-i18next";
-import { useChangeLanguage } from "remix-i18next/react";
-import i18next from "./localization/i18n.server";
-import styles from "~/tailwind.css?url";
+import {
+  type LinksFunction,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useLoaderData,
+} from 'react-router'
+import { useTranslation } from 'react-i18next'
+import { useChangeLanguage } from 'remix-i18next/react'
+import i18next from './localization/i18n.server'
+import styles from '~/tailwind.css?url'
 
-export async function loader({ context, request }: { context: any; request: Request }) {
-  const { lang, clientEnv } = context;
-  const locale = await i18next.getLocale(request);
-  return { lang, clientEnv, locale };
+export async function loader({
+  context,
+  request,
+}: {
+  context: any
+  request: Request
+}) {
+  const { lang, clientEnv } = context
+  const locale = await i18next.getLocale(request)
+  return { lang, clientEnv, locale }
 }
 
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: styles },
-];
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }]
 
 export const handle = {
-  i18n: "common",
-};
+  i18n: 'common',
+}
 
 export default function App() {
-  const { locale } = useLoaderData<typeof loader>();
-  const { i18n } = useTranslation();
+  const { locale } = useLoaderData<typeof loader>()
+  const { i18n } = useTranslation()
 
-  useChangeLanguage(locale);
+  useChangeLanguage(locale)
 
   return (
-    <html className="overflow-y-auto overflow-x-hidden" lang={i18n.language} dir={i18n.dir()}>
+    <html
+      className="overflow-y-auto overflow-x-hidden"
+      lang={i18n.language}
+      dir={i18n.dir()}
+    >
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -38,5 +54,5 @@ export default function App() {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import { reactRouterHonoServer } from "react-router-hono-server/dev";
-import { iconsSpritesheet } from "vite-plugin-icons-spritesheet";
-import tsConfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite'
+import { defineConfig } from 'vite'
+import { reactRouterHonoServer } from 'react-router-hono-server/dev'
+import { reactRouterDevTools } from 'react-router-devtools'
+import { iconsSpritesheet } from 'vite-plugin-icons-spritesheet'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [
-        reactRouter(),
-        tsConfigPaths(),
-        reactRouterHonoServer(),
-        iconsSpritesheet(
-            {
-                withTypes: true,
-                inputDir: "icons",
-                outputDir: "./public/icons",
-                fileName: "icons.svg",
-                formatter: "prettier",
-            }
-        ),
-    ],
+  plugins: [
+    tsConfigPaths(),
+    reactRouterHonoServer(),
+    reactRouterDevTools(),
+    reactRouter(),
+    iconsSpritesheet({
+      withTypes: true,
+      inputDir: 'icons',
+      outputDir: './public/icons',
+      fileName: 'icons.svg',
+      formatter: 'prettier',
+    }),
+  ],
 })
