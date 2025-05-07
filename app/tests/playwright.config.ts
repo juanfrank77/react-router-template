86c39469c76bfb7ba14379553286155d0a4127d6
Juan Gonzalez <seminariopv@hotmail.com>
2025-05-07 05:45:01 -0600

    Add setup for visual testing

---
+++ app/tests/playwright.config.ts
@@ -0,0 +1,39 @@
+import os from 'os'
+import path from 'path'
+import { defineConfig } from '@playwright/test'
+
+const PORT = process.env.PORT || '3000'
+
+const tmpDir = path.join(os.tmpdir(), 'react-router-template')
+
+export default defineConfig({
+    timeout: 10000 * (process.env.CI ? 10 : 1),
+    expect: {
+        timeout: 5000 * (process.env.CI ? 10 : 1)
+    },
+    outputDir: path.join(tmpDir, 'playwright-test-output'),
+    reporter: [
+        [
+            'html',
+            { open: 'never', outputFolder: path.join(tmpDir, 'playwright-report') }
+        ],
+    ],
+    use: {
+        baseURL: `http://localhost:${PORT}/`,
+        trace: 'retain-on-failure'
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: 'Desktop Chrome'
+        },
+    ],
+    webServer: {
+        command: 'pnpm dev',
+        port: Number(PORT),
+        reuseExistingServer: !process.env.CI,
+        stdout: 'pipe',
+        stderr: 'pipe',
+        env: { PORT }
+    }
+})
